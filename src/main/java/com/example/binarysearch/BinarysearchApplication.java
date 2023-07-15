2a1d4cb57fba6d094fbec445bf5dbe8ed0f61fcd
Tomáš Čížek <tomascizek@gmail.com>
2023-07-16 00:18:11 +0200

    init spring maven project

---
+++ src/main/java/com/example/binarysearch/BinarysearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.binarysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarysearchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BinarysearchApplication.class, args);
+  }
+
+}
